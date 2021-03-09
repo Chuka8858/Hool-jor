df227e6dbffb23d972be63d325f8f202c4f9b438
Chuka8858 <chukachuka993@gmail.com>
2021-03-09 14:30:11 +0800

    3rd commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,32 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
 
-let search = new Search("pasta");
+// web app status
+// hailt query ur dun
+// uzuulj baiga jor
+// liked jor
+// ordered joriin nairlaga
 
-search.doSearch().then((r) => console.log(r));
+const state = {};
+
+const controlSearch = async () => {
+  //   1 => webees hailtynh tulhur ug gargaj awna
+  const query = "pizza";
+
+  if (query) {
+    //  2 => shine hailtyn object uusgej ogno
+    state.search = new Search(query);
+
+    // 3 => hailt hihihed zoruilj delgetsiin UI beltgene
+
+    // 4 => hailtyg guitsetgene
+    await state.search.doSearch();
+    // 5 => hailtyn ur dung delgetsend guitsetgene
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
