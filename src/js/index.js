d72f88b563750dc942d38562a8866360bc3b9a25
Chuka8858 <chukachuka993@gmail.com>
2021-03-09 17:19:45 +0800

    loading page neew

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ require("@babel/polyfill");
 import Search from "./model/Search";
 import { elements, renderLoader, clearLoader } from "./view/base";
 import * as searchView from "./view/searchView";
-
+import Recipe from "./model/Recipe";
 // web app status
 // hailt query ur dun
 // uzuulj baiga jor
@@ -37,3 +37,15 @@ elements.searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   controlSearch();
 });
+
+elements.pageButtons.addEventListener("click", (e) => {
+  const btn = e.target.closest(".btn-inline");
+
+  if (btn) {
+    const gotoPageNumber = parseInt(btn.dataset.goto, 10);
+    searchView.clearSearchResult();
+    searchView.renderRecipes(state.search.result, gotoPageNumber);
+  }
+});
+const r = new Recipe(47746);
+r.getRecipe();
