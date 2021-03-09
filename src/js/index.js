22db0bf7685fc812b76224357b8ab8bf5667501a
Chuka8858 <chukachuka993@gmail.com>
2021-03-09 15:54:20 +0800

    search icon

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
+import { elements, renderLoader, clearLoader } from "./view/base";
+import * as searchView from "./view/searchView";
 
 // web app status
 // hailt query ur dun
@@ -11,22 +13,27 @@ const state = {};
 
 const controlSearch = async () => {
   //   1 => webees hailtynh tulhur ug gargaj awna
-  const query = "pizza";
+  const query = searchView.getInput();
 
   if (query) {
     //  2 => shine hailtyn object uusgej ogno
     state.search = new Search(query);
 
     // 3 => hailt hihihed zoruilj delgetsiin UI beltgene
+    searchView.clearSearchQuery();
+    searchView.clearSearchResult();
+    renderLoader(elements.searchResultDiv);
 
     // 4 => hailtyg guitsetgene
     await state.search.doSearch();
     // 5 => hailtyn ur dung delgetsend guitsetgene
-    console.log(state.search.result);
+    clearLoader();
+    if (state.search.result === undefined) alert("Hailtaar ilertsgui");
+    else searchView.renderRecipes(state.search.result);
   }
 };
 
-document.querySelector(".search").addEventListener("submit", (e) => {
+elements.searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   controlSearch();
 });
--- src/js/model/Search.js
+++ src/js/model/Search.js
@@ -15,7 +15,7 @@ export default class Search {
 
       return this.result;
     } catch (error) {
-      alert("asuudal garlaa" + error);
+      console.log("asuudal garlaa" + error);
     }
   }
 }
